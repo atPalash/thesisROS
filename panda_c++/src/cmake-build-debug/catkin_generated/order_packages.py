837e90e5450df32c4af87c636191b392e462977d
palash <mpalash.halder@gmail.com>
2019-02-08 19:28:50 +0200

    ROS setup

---
+++ panda_c++/src/cmake-build-debug/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/palash/thesis/thesisROS/panda_c++/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/palash/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/palash/catkin_ws/devel;/opt/ros/kinetic" != "" else []
